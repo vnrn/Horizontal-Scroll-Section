a2e409fae8a9f7c570faebac9ffba9b95a7cdd2a
vnun <vnun.b2b@gmail.com>
2024-11-17 18:34:44 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head"
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Horizontal scroll",
+  description: "Next js Horizontal scroll Assets",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <Head>
+        import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +35,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
